741bd816bbb4247d8ea3608c21d07e3a6a5f2c77
Naim Boff <usertcm413@gmail.com>
2020-12-09 00:59:03 -0300

    Initial commit

---
+++ FileControlUtility/TransferSettings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace FileControlUtility
+{
+    public enum FileNameConflictMethod
+    {
+        DO_NOT_MOVE = 0,
+        REPLACE_ALL = 1,
+        REPLACE_DIFFERENT = 2,
+        RENAME_DIFFERENT = 3
+    }
+
+    public enum SpecifiedFileNamesAndExtensionsMode
+    {
+        ALLOW_ONLY,
+        IGNORE
+    }
+
+    public class TransferSettings
+    {
+        public string SourcePath { get; set; }
+        public string DestinyPath { get; set; }
+        public bool MoveSubFolders { get; set; }
+        public bool KeepOriginFiles { get; set; }
+        public bool CleanDestinyDirectory { get; set; }
+        public bool DeleteUncommonFiles { get; set; }
+        public bool AllowIgnoreFileExt { get; set; }
+        public FileNameConflictMethod FileNameConflictMethod { get; set; }
+        public SpecifiedFileNamesAndExtensionsMode SpecifiedFileNamesOrExtensionsMode { get; set; }
+        public List<string> SpecifiedFileNamesAndExtensions { get; set; }
+    }
+}
